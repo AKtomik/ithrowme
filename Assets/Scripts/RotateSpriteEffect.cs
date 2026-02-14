cdcf461b7e7a9ba5ddcc59aa76f4dc725e82b3d1
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-02-14 17:49:31 +0100

    trhowable rotatable rotate well

--- Assets/Scripts/RotateSpriteEffect.cs
+++ Assets/Scripts/RotateSpriteEffect.cs
@@ -23,14 +23,13 @@ public class RotateSpriteEffect : MonoBehaviour
     {
         Vector3 angleSpin = spriteTransform.rotation.eulerAngles;
         Vector3 angleLook = spriteTransform.rotation.eulerAngles + transform.rotation.eulerAngles;
-        angleLook = new Vector3(angleLook.x % 360, angleLook.y % 360, angleLook.z %360);
 
-        Vector3 angle = angleSpin + angleLook;
+        Vector3 angle = angleLook;
         angle = new Vector3(angle.x % 360, angle.y % 360, angle.z % 360) / 360;
 
         Sprite angleSprite = spritesImage[(int)((1 - angle.y) * spritesImage.Length)];
         spriteRender.sprite = angleSprite;
 
-        transform.Rotate(new Vector3(0, 3 * Time.deltaTime, 0));
+        transform.Rotate(new Vector3(0, 60 * Time.deltaTime, 0));
     }
 }
