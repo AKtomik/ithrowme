a143615e871b752639229f374cb55ef2742872bc
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-02-14 14:33:36 +0100

    rotate combine spin and look

--- Assets/RotateSpriteEffect.cs
+++ Assets/RotateSpriteEffect.cs
@@ -21,8 +21,16 @@ public class RotateSpriteEffect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float angle = Quaternion.Angle(cameraTransform.rotation, spriteTransform.rotation); 
-        Sprite angleSprite = spritesImage[(int)(angle/180 * spritesImage.Length)];
+        Vector3 angleSpin = spriteTransform.rotation.eulerAngles;
+        Vector3 angleLook = spriteTransform.rotation.eulerAngles + transform.rotation.eulerAngles;
+        angleLook = new Vector3(angleLook.x % 360, angleLook.y % 360, angleLook.z %360);
+
+        Vector3 angle = angleSpin + angleLook;
+        angle = new Vector3(angle.x % 360, angle.y % 360, angle.z % 360) / 360;
+
+        Sprite angleSprite = spritesImage[(int)((1 - angle.y) * spritesImage.Length)];
         spriteRender.sprite = angleSprite;
+
+        transform.Rotate(new Vector3(0, 3 * Time.deltaTime, 0));
     }
 }
