0396b0458b9ac82a51239c3ad004d8b2045140f3
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-02-25 12:51:38 +0100

    working laggy script

--- Assets/Scripts/LaggyEffect.cs
+++ Assets/Scripts/LaggyEffect.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class LaggyEffect : MonoBehaviour
 {
+    
+    [SerializeField] private Transform renderTransform;
+    [SerializeField] private Transform realTransform;
+
+    public float renderFps = 10;
+    private float renderInterval;
+    private float renderTime = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        renderInterval = 1 / renderFps;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        renderTime += Time.deltaTime;
+        if (renderTime > renderInterval)
+        {
+            RefreshRender();
+            renderTime = 0;
+        }
+        UpdateRender();
+    }
+
+    void UpdateRender()
+    {
+        renderTransform.position = realTransform.position;
+    }    
+
+    void RefreshRender()
+    {
+        renderTransform.rotation = realTransform.rotation;
     }
 }
