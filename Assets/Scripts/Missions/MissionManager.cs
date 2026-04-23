d9270b4d89215e3db4df24b0eb8bd046c04990f7
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-04-23 15:52:06 +0200

    mission manager

---
+++ Assets/Scripts/Missions/MissionManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+
+public class MissionManager : MonoBehaviour
+{
+    public GameObject MissionTextPrefab;// prefab with a TextMeshProUGUI
+    public Transform MissionUiContainer;// vertical layout group
+    
+    private Dictionary<int, string> missionsDictionnary = new Dictionary<int, string>(); // list of all the missions
+    private List<int> activeMissions = new List<int>(); // list of the active missions (the ones who are displayed)
+    private List<int> finishMissions = new List<int>(); // list of the completed missions
+
+    private List<GameObject> spawnedTextMissions = new List<GameObject>(); // every missions displayed
+
+    public void AddMission(int id)
+    {
+        if (!activeMissions.Contains(id)) // verify if the mission is already displayed
+        {
+            activeMissions.Add(id);
+            RefreshMissions(); // add the id to the list and refresh the canva
+        }
+    }
+
+    public void CompleteMission(int id) // remove the id from the list then refresh (mission disappears)
+    {
+        if (activeMissions.Contains(id))
+        {
+            activeMissions.Remove(id);
+            if (!finishMissions.Contains(id))
+            {
+                finishMissions.Add(id);
+            }
+            RefreshMissions();
+        }
+    }
+
+    private List<string> GetActiveMissionTexts()
+    {
+        return GetTexts(activeMissions);
+    }
+
+    private List<string> GetFinishMissionTexts()
+    {
+        return GetTexts(finishMissions);
+    }
+    
+    private List<string> GetTexts(List<int> missionsId)
+    {
+        List<string> texts = new List<string>();
+        foreach (int id in missionsId)
+        {
+            if (missionsDictionnary.ContainsKey(id))
+                texts.Add(missionsDictionnary[id]);
+        }
+        return texts; // for each id, return their equivalent mission texts 
+    }
+
+    public void RefreshMissions()
+    {
+        // destroy old prefab before create new ones
+        foreach (GameObject obj in spawnedTextMissions)
+            Destroy(obj);
+        spawnedTextMissions.Clear();
+
+        // Instancier un prefab par mission active
+        foreach (string text in GetActiveMissionTexts())
+        {
+            GameObject instance = Instantiate(MissionTextPrefab, MissionUiContainer); // for every active mission : instantiate
+            instance.GetComponentInChildren<TextMeshProUGUI>().text = text; // write text
+            spawnedTextMissions.Add(instance); // add to spawnedMissions
+        }
+    }
+
+    private void Start()
+    {
+        missionsDictionnary.Add(1, "Réparer le générateur");
+        missionsDictionnary.Add(2, "Trouver le fusible");
+        missionsDictionnary.Add(3, "Parler au gardien");
+
+        // Démarrer avec 2 missions simultanées
+        AddMission(1);
+        AddMission(2);
+    }
+}
