d76e50370f62137e6b5a5105feed7f9f14918f65
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-04-26 14:02:53 +0200

    tag 'Items'

--- Assets/Scripts/Doors/ButtonScript.cs
+++ Assets/Scripts/Doors/ButtonScript.cs
@@ -22,7 +22,7 @@ public class ButtonScript : MonoBehaviour
     {
         if (doorScript)
         {
-            if (collision.gameObject.CompareTag("throwable_item"))  ///// the button is pressed ////
+            if (collision.gameObject.CompareTag("Items"))  ///// the button is pressed ////
             {
                 doorScript.OpeningDoors();
                 //// destroy item ////
