60f11bf57b6a88f26852bf95542a4409cd65ed49
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-04-28 19:30:51 +0200

    add timer caus its cool

---
+++ Assets/Scripts/Items/TimerScript.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Timers;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public bool running = false;
+    private double timedSec = 0;
+    private TextMeshProUGUI textPro;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        textPro = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!running) return;
+        timedSec += Time.deltaTime;
+
+        double displayMs = Math.Floor(timedSec*1000)%1000;
+        string textMs = displayMs.ToString();
+
+        double displaySec = Math.Floor(timedSec);
+        string textSec = displaySec.ToString();
+        
+        textPro.text = textSec + "." + textMs;
+    }
+}
