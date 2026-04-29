b1a9dc43b38f2b627af03ac5b5a7eef3d932dc49
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-04-29 09:49:51 +0200

    set noconsiderationspeed

--- Assets/Scripts/ClampedVelocity.cs
+++ Assets/Scripts/ClampedVelocity.cs
@@ -7,6 +7,9 @@ public class ClampedVelocity : MonoBehaviour
     public bool clampMax = false;
     public float speedMax = 10f;
 
+    public bool stopBellowNoConsideration = true;
+    public float noConsiderationSpeed = .000001f;
+
     private Rigidbody rb;
 
     void Awake()
@@ -19,6 +22,8 @@ public class ClampedVelocity : MonoBehaviour
         if (rb.linearVelocity == Vector3.zero) return;// do nothing when no direction
         
         float speed = rb.linearVelocity.magnitude;
+        if (stopBellowNoConsideration && speed <= noConsiderationSpeed) return;
+
         if (clampMin && speed < speedMin)
         {
             rb.linearVelocity = rb.linearVelocity.normalized * speedMin;
