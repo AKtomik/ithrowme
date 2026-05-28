3fc0a3ae2ef6e1b1129960b7d0e0e1b58c34908e
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-05-28 23:54:12 +0200

    one time button trigger settings

--- Assets/Scripts/Missions/ButtonTrigger.cs
+++ Assets/Scripts/Missions/ButtonTrigger.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 public abstract class ButtonTrigger : MonoBehaviour
 {
+    public bool ONE_TIME_TRIGGER = true;
+    public Vector3 TRIGGER_SCALE = new Vector3(1, 1, 1/4);
+
     private void OnCollisionEnter(Collision other)
     {
         if (!this.enabled || !other.gameObject.CompareTag("Items")) return;
         OnTrigger();
-        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z / 4);
-        this.enabled = false;
+        transform.localScale = new Vector3(transform.localScale.x * TRIGGER_SCALE.x, transform.localScale.y * TRIGGER_SCALE.y, transform.localScale.z * TRIGGER_SCALE.z);
+        if (ONE_TIME_TRIGGER)
+        {
+            this.enabled = false;
+        }
     }
 
     public abstract void OnTrigger();
