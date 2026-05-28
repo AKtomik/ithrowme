7ef11b6cfc952428756ca497e99882536ed626dc
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-05-28 23:59:32 +0200

    step one lever enable an item

--- Assets/Scripts/Missions/Steps/Step1Lever.cs
+++ Assets/Scripts/Missions/Steps/Step1Lever.cs
@@ -4,13 +4,13 @@ public class Step1Lever : TakableLever
 {
     public DoorOpeningScript doorScript;
     public MissionManager missionManager;
-    public TakableItem enableItem;
+    public GameObject activatedItem;
 
     public override void OnTrigger(CapsulePlayer player) {
         Debug.Log("Step1Button: step 1 completed");
         doorScript.OpeningDoors();
         missionManager.CompleteMission(1);
         missionManager.AddMission(2);
-        enableItem.enabled = true;
+        activatedItem.SetActive(true);
     }
 }
