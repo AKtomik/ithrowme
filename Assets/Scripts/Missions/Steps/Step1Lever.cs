2bb7d403d676a38c7d1a80b26b193813c62b7bd3
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-05-28 23:39:54 +0200

    setep 1 button is now a lever

--- Assets/Scripts/Missions/Steps/Step1Lever.cs
+++ Assets/Scripts/Missions/Steps/Step1Lever.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class Step1Button : ButtonTrigger
+public class Step1Lever : TakableLever
 {
     public DoorOpeningScript doorScript;
     public MissionManager missionManager;
 
-    public override void OnTrigger() {
+    public override void OnTrigger(CapsulePlayer player) {
         Debug.Log("Step1Button: step 1 completed");
         doorScript.OpeningDoors();
         missionManager.CompleteMission(1);
--- Assets/Scripts/Takables/TakableLever.cs
+++ Assets/Scripts/Takables/TakableLever.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 
-public class TakableLever : Takable
+abstract public class TakableLever : Takable
 {
+    public bool TAKE_STOP_VELOCITY = true;
+
     override public void Take(CapsulePlayer player)
     {
-        // stop
-        player.playerBody.linearVelocity = Vector3.zero;
-        player.playerBody.angularVelocity = Vector3.zero;
+        if (TAKE_STOP_VELOCITY)
+        {
+            player.playerBody.linearVelocity = Vector3.zero;
+            player.playerBody.angularVelocity = Vector3.zero;
+        }
+        OnTrigger(player);
     }
     
-    override public void Throw(CapsulePlayer player)
-    {
-    }
+    override public void Throw(CapsulePlayer player) {}
+
+    abstract public void OnTrigger(CapsulePlayer player);
 }
