d0fc449caa241901583a12f8041e68f31f5e5736
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-05-29 09:01:08 +0200

    readded step 1 button

--- Assets/Scripts/Missions/Steps/Step1Button.cs
+++ Assets/Scripts/Missions/Steps/Step1Button.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
 
-public class Step1Lever : TakableLever
+public class Step1Button : ButtonTrigger
 {
     public DoorOpeningScript doorScript;
     public MissionManager missionManager;
-    public GameObject activatedItem;
 
-    public override void OnTrigger(CapsulePlayer player) {
-        Debug.Log("Step1Lever: step 1 completed");
+    public override void OnTrigger() {
+        Debug.Log("Step1Button: step 1 completed");
         doorScript.OpeningDoors();
         missionManager.CompleteMission(1);
         missionManager.AddMission(2);
-        activatedItem.SetActive(true);
     }
 }
