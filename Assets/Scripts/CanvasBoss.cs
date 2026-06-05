3f53f121b1736f809f4447228c823ee98d9746af
Ange BERTRAND <ange.tigrou2007@gmail.com>
2026-06-06 01:28:38 +0200

    fixed layout problem

--- Assets/Scripts/CanvasBoss.cs
+++ Assets/Scripts/CanvasBoss.cs
@@ -6,20 +6,6 @@ public class CanvasBoss : MonoBehaviour
     [SerializeField] private GameObject cinematicBars;
     [SerializeField] private GameObject missionsContainer;
 
-
-
-    private void Start()
-    {
-        Canvas.ForceUpdateCanvases();
-
-    }
-    private void Update()
-    {
-        Canvas.ForceUpdateCanvases();
-        LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
-        LayoutRebuilder.ForceRebuildLayoutImmediate(missionsContainer.GetComponent<RectTransform>());
-    }
-
     public void EnableCinematic()
     {// TODO: bar progressive enter with time parameter
         cinematicBars.SetActive(true);
