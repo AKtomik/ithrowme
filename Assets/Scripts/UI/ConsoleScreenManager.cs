53baefc3f094e03f20d823f8bcafd000455a5351
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-06-08 16:41:25 +0200

    init screen

--- Assets/Scripts/UI/ConsoleScreenManager.cs
+++ Assets/Scripts/UI/ConsoleScreenManager.cs
@@ -7,17 +7,21 @@ public class ConsoleScreenManager : MonoBehaviour
     [Header("Conole texts")]
     [SerializeField] private LayoutGroup textContainer;
     [SerializeField] private GameObject textPrefab;
+    [SerializeField] private bool doInitText;
+    [SerializeField, DrawIf("doInitText")] private string initText;
     
     [Header("Conole screen")]
     [SerializeField] private MeshRenderer screenMesh;
+    [SerializeField] private bool doInitScreenColor;
+    [SerializeField, DrawIf("doInitScreenColor")] private Color initScreenColor;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        AddText("hello world"); 
-        AddText("fake long unity error with multiples lines", Color.red);
-        AddText("godot is better!", Color.gold);
-        ChangeScreenColor(Color.gold);
+        if (doInitText)
+            AddText(initText); 
+        if (doInitScreenColor)
+            ChangeScreenColor(initScreenColor);
     }
 
     // Update is called once per frame
