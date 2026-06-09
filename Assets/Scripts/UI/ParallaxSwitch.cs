0e3c6c0ec2a8b670de39b7a336bb24e3808b7e34
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-06-10 01:56:25 +0200

    parallax swith enable empty ghost

--- Assets/Scripts/UI/ParallaxSwitch.cs
+++ Assets/Scripts/UI/ParallaxSwitch.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class ParallaxSwitch : MonoBehaviour
 {
     private GameObject[] farAwayObjects = new GameObject[] {};
+    [SerializeField] private GameObject parallaxParent;
+    [SerializeField] private bool disableDisable = false;
 
 	void Awake()
 	{
@@ -11,17 +13,17 @@ public class ParallaxSwitch : MonoBehaviour
 
 	private void OnTriggerEnter()
     {
-        foreach (var obj in farAwayObjects)
-        {
-            obj.SetActive(false);
-        }
+        if (!disableDisable)
+            foreach (var obj in farAwayObjects)
+                obj.SetActive(false);
+        parallaxParent.SetActive(true);
     }
     
     private void OnTriggerExit(Collider other)
     {
-        foreach (var obj in farAwayObjects)
-        {
-            obj.SetActive(true);
-        }
+        if (!disableDisable)
+            foreach (var obj in farAwayObjects)
+                obj.SetActive(true);
+        parallaxParent.SetActive(false);
     }
 }
