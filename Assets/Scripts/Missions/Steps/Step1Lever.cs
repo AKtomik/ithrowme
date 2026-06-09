458c9a2359d891f76e164788d856bdec8af54c51
Ange BERTRAND <ange.tigrou2007@gmail.com>
2026-06-09 22:14:38 +0200

    add alarms

--- Assets/Scripts/Missions/Steps/Step1Lever.cs
+++ Assets/Scripts/Missions/Steps/Step1Lever.cs
@@ -6,13 +6,33 @@ public class Step1Lever : TakableLever
     public MissionManager missionManager;
     public GameObject activatedItem;
 
+    private AudioSource[] alarmsAudio;
+
+    private void Awake()
+    {
+        GameObject[] alarmObjects = GameObject.FindGameObjectsWithTag("Alarm");
+
+
+        var sources = new System.Collections.Generic.List<AudioSource>();
+        foreach (GameObject obj in alarmObjects)
+            sources.AddRange(obj.GetComponents<AudioSource>());
+
+        alarmsAudio = sources.ToArray();
+
+        foreach (AudioSource alarm in alarmsAudio)
+            alarm.Play();
+    }
     public override void PullStart(CapsulePlayer player) {
         Debug.Log("Step1Lever: pulling...");
         missionManager.CompleteMission(1);
+
+
     }
     
     public override void PullFinish(CapsulePlayer player) {
         Debug.Log("Step1Lever: step 1 completed");
+        foreach (AudioSource alarm in alarmsAudio)
+            alarm.Stop();
         ReferenceStore.instance.emergencyLifeDoor.OpeningDoors();
         missionManager.AddMission(2);
         activatedItem.SetActive(true);
