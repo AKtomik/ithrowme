d136df12a0ac154de73b9e9972e29a64bf685ca3
Ange BERTRAND <ange.tigrou2007@gmail.com>
2026-06-09 22:26:15 +0200

    alarm is safe in script

--- Assets/Scripts/Missions/Steps/Step1Lever.cs
+++ Assets/Scripts/Missions/Steps/Step1Lever.cs
@@ -12,15 +12,18 @@ public class Step1Lever : TakableLever
     {
         GameObject[] alarmObjects = GameObject.FindGameObjectsWithTag("Alarm");
 
+        if (alarmObjects.Length > 0 )
+        {
+            var sources = new System.Collections.Generic.List<AudioSource>();
+            foreach (GameObject obj in alarmObjects)
+                sources.AddRange(obj.GetComponents<AudioSource>());
 
-        var sources = new System.Collections.Generic.List<AudioSource>();
-        foreach (GameObject obj in alarmObjects)
-            sources.AddRange(obj.GetComponents<AudioSource>());
+            alarmsAudio = sources.ToArray();
 
-        alarmsAudio = sources.ToArray();
+            foreach (AudioSource alarm in alarmsAudio)
+                alarm.Play();
+        }
 
-        foreach (AudioSource alarm in alarmsAudio)
-            alarm.Play();
     }
     public override void PullStart(CapsulePlayer player) {
         Debug.Log("Step1Lever: pulling...");
@@ -31,8 +34,12 @@ public class Step1Lever : TakableLever
     
     public override void PullFinish(CapsulePlayer player) {
         Debug.Log("Step1Lever: step 1 completed");
-        foreach (AudioSource alarm in alarmsAudio)
-            alarm.Stop();
+        if (alarmsAudio.Length > 0)
+        {
+            foreach (AudioSource alarm in alarmsAudio)
+                alarm.Stop();
+        }
+
         ReferenceStore.instance.emergencyLifeDoor.OpeningDoors();
         missionManager.AddMission(2);
         activatedItem.SetActive(true);
