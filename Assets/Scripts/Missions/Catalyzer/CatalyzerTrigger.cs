49a31fe133183d8a804b6cc0d9c9085372d01b64
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-06-14 05:44:08 +0200

    cube rotatation

--- Assets/Scripts/Missions/Catalyzer/CatalyzerTrigger.cs
+++ Assets/Scripts/Missions/Catalyzer/CatalyzerTrigger.cs
@@ -1,28 +1,47 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public abstract class CatalyzerTrigger : MonoBehaviour
 {
+    [Header("Catalyzer Trigger")]
     public bool oneTimeTrigger = true;
     public Transform putTransform;
+    [Header("Catalyzer Rotation")]
+    public float rotationMinSpeed = 470f;
+    public float rotationStartSpeed = 1470f;
+    public float rotationSlowSpeed = 200f;
+    
+    private float speed = 0;
+    private Collider collid;
 
-    public void OnTriggerEnter(Collider other)
+	public void Start()
+	{
+        collid = GetComponent<Collider>();
+        speed = 0;
+	}
+
+	public void Update()
+	{
+        if (speed == 0) return;
+        if (speed > rotationMinSpeed) speed -= rotationSlowSpeed * Time.deltaTime;
+		putTransform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+	}
+
+	public void OnTriggerEnter(Collider other)
 	{
         // check
-        if (!this.enabled) return;
-        Debug.Log("hello in", other);
+        if (!enabled) return;
         if (!other.gameObject.TryGetComponent(out CatalyzableItem catalyzable)) return;
-        Debug.Log("catalyzable took", catalyzable);
         if (!other.gameObject.TryGetComponent(out TakableReference takableReference)) return;
-        Debug.Log("takableReference took", takableReference);
         var takableItem = takableReference.takable as TakableItem;
-        Debug.Log("takableItem took", takableItem);
         
         // put
         takableItem.Put(putTransform);
+        speed = rotationStartSpeed;
 
         // trigger
         OnTrigger();
-        if (oneTimeTrigger) this.enabled = false;
+        if (oneTimeTrigger) collid.enabled = false;
     }
 
     public abstract void OnTrigger();
