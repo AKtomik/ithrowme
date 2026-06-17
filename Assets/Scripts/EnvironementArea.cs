8b2acc4a6a839912f95cce2361bae3cd4a7fe224
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-06-17 20:53:31 +0200

    trigger is hidding

--- Assets/Scripts/EnvironementArea.cs
+++ Assets/Scripts/EnvironementArea.cs
@@ -4,6 +4,7 @@ public class EnvironementArea : MonoBehaviour
 {
   [SerializeField] private GameObject environementParent;
   [SerializeField] private bool defaultHide = false;
+  [SerializeField] private bool triggerIsHidding = false;
 
 	private bool showning;
 	//private bool oldState;
@@ -16,13 +17,15 @@ public class EnvironementArea : MonoBehaviour
 	void OnTriggerEnter(Collider other)
 	{
 		if (!other.gameObject.CompareTag("Player")) return;
-		SetShow(true);
+		Debug.Log("true ^ triggerIsHidding = "+true+triggerIsHidding+(true ^ triggerIsHidding));
+		SetShow(true ^ triggerIsHidding);
 	}
 	
 	void OnTriggerExit(Collider other)
 	{
 		if (!other.gameObject.CompareTag("Player")) return;
-		SetShow(false);
+		Debug.Log("false ^ triggerIsHidding = "+false+triggerIsHidding+(false ^ triggerIsHidding));
+		SetShow(false ^ triggerIsHidding);
 	}
 
 	void SetShow(bool doShow)
