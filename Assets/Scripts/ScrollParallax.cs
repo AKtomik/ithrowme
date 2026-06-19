f0ef225ac1d8ad61311b8cb43965d7ebdd82adc5
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-06-19 20:24:44 +0200

    scroll parallax in fourth direction

--- Assets/Scripts/ScrollParallax.cs
+++ Assets/Scripts/ScrollParallax.cs
@@ -5,29 +5,57 @@ public class ScrollParallax : MonoBehaviour
     [SerializeField] private RectTransform canvasChild;
     private GameObject canvasUp;
     private GameObject canvasDown;
-    private float height;
+    private float dist;
+    private Vector3 dir;
     private float progress = 1f;
-    private int step;
 
-    [SerializeField] private bool scrolling = true;
-    [SerializeField] private float scrollSpeed = 1;
+    public bool scrolling = true;
+    public ScrollParallaxDirection scrollDirection = ScrollParallaxDirection.DOWN;
+    private ScrollParallaxDirection cachedDirection;
+    public float scrollSpeed = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        height = canvasChild.rect.height * canvasChild.lossyScale.y;
+        UpdateDirection();
 
         canvasDown = canvasChild.gameObject;
         canvasDown.transform.position = transform.position;
 
         canvasUp = Instantiate(canvasChild.gameObject, canvasChild.parent);// [transform] should be equal to [canvasChild.parent]
-        canvasUp.transform.position = transform.position - canvasChild.transform.up * height;
+        canvasUp.transform.position = transform.position + dir * dist;
+    }
+
+    void UpdateDirection()
+    {
+        cachedDirection = scrollDirection;
+        switch (scrollDirection)
+        {
+            case ScrollParallaxDirection.UP: {
+                dir = canvasChild.transform.up; 
+                dist = canvasChild.rect.height * canvasChild.lossyScale.y;
+            } break;
+            case ScrollParallaxDirection.DOWN: {
+                dir = -canvasChild.transform.up; 
+                dist = canvasChild.rect.height * canvasChild.lossyScale.y;
+            } break;
+            case ScrollParallaxDirection.RIGHT: {
+                dir = canvasChild.transform.right; 
+                dist = canvasChild.rect.width * canvasChild.lossyScale.y;
+            } break;
+            case ScrollParallaxDirection.LEFT: {
+                dir = -canvasChild.transform.right; 
+                dist = canvasChild.rect.width * canvasChild.lossyScale.y;
+            } break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!scrolling) return;
+
+        if (cachedDirection != scrollDirection) UpdateDirection();
         
         progress += Time.deltaTime / 10 * scrollSpeed;
         if (progress > 1)
@@ -39,7 +67,15 @@ public class ScrollParallax : MonoBehaviour
             progress = 1;
         }
 
-        canvasDown.transform.position = transform.position + ((1 - progress) * height * canvasDown.transform.up);
-        canvasUp.transform.position = transform.position - (progress * height * canvasDown.transform.up);
+        canvasDown.transform.position = transform.position - ((1 - progress) * dist * dir);
+        canvasUp.transform.position = transform.position + (progress * dist * dir);
     }
+}
+
+public enum ScrollParallaxDirection
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
 }
