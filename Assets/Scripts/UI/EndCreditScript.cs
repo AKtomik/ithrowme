8fa1be1fea4c982b12460f36e8941e5898b4beae
Ange BERTRAND <ange.tigrou2007@gmail.com>
2026-06-20 15:29:20 +0200

    transition to MainMenuLevel

--- Assets/Scripts/UI/EndCreditScript.cs
+++ Assets/Scripts/UI/EndCreditScript.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndCreditScript : MonoBehaviour
 {
     [SerializeField] private float scrollSpeed = 40f;
+    [SerializeField] private float fadeDuration = 2f;
     [SerializeField] private RectTransform creditTransform;
     [SerializeField] private AudioSource creditAudio;
+    [SerializeField] private RawImage endBlackScreen;
     private bool isEndind = false;
     private void Start()
     {
         creditTransform.gameObject.SetActive(false);
+
+        Color color = endBlackScreen.color;
+        color.a = 0f;
+        endBlackScreen.color = color;
     }
 
 
@@ -26,5 +35,37 @@ public class EndCreditScript : MonoBehaviour
         creditTransform.gameObject.SetActive(true);
         isEndind = true;
         creditAudio.Play();
+        Invoke("EndingT", 32);
     }
+
+    public void EndingT()
+    {
+        StartCoroutine(FadeToBlack());
+    }
+
+    private IEnumerator FadeToBlack()
+    {
+        float timer = 0f;
+
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+
+            Color color = endBlackScreen.color;
+            color.a = Mathf.Clamp01(timer / fadeDuration);
+            endBlackScreen.color = color;
+
+            yield return null;
+        }
+
+        Color finalColor = endBlackScreen.color;
+        finalColor.a = 1f;
+        endBlackScreen.color = finalColor;
+
+        yield return new WaitForSeconds(2);
+
+        SceneManager.LoadScene("MainMenuLevel");
+    }
+
+    
 }
