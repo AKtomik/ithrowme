9261b31b0326fd07235ccf8ac4452ebc142d914e
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-06-21 16:15:56 +0200

    area items exit first fix

--- Assets/Scripts/EnvironementArea.cs
+++ Assets/Scripts/EnvironementArea.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnvironementArea : MonoBehaviour
@@ -7,11 +9,19 @@ public class EnvironementArea : MonoBehaviour
   [SerializeField] private bool defaultHide = false;
   [SerializeField] private bool triggerIsHidding = false;
 
+	private Dictionary<Renderer, bool> renderers = new();
 	private bool showning;
-	//private bool oldState;
 
 	void Start()
 	{
+	  //renderers = environementParent.GetComponentsInChildren<Behaviour>(true);
+	  //renderers = new Behaviour[] {};
+		foreach (var render in environementParent.GetComponentsInChildren<Renderer>(true))
+		{
+			renderers[render] = render.enabled;
+		}
+		//renderers.AddRange(environementParent.GetComponentsInChildren<Canvas>(true));
+		//Debug.Log("renderers:"+renderers.Count);
 		SetShow(!defaultHide);
 	}
 
@@ -29,8 +39,36 @@ public class EnvironementArea : MonoBehaviour
 
 	void SetShow(bool doShow)
 	{
+		//Debug.Log("SetShow:"+renderers.Count+"of"+doShow);
+		if (showning == doShow) return;
 		showning = doShow;
-		environementParent.SetActive(doShow);
+
+		foreach(var key in renderers.Keys.ToArrayPooled())
+		{
+			if(key == null)
+			{
+				renderers.Remove(key);
+			}
+		}
+
+		if (doShow)
+			foreach (var render in renderers.Keys.ToArrayPooled())
+			{
+				if (render)
+				{
+					render.enabled = renderers[render];
+				}
+			}
+		else
+			foreach (var render in renderers.Keys.ToArrayPooled())
+			{
+				if (render)
+				{
+					renderers[render] = render.enabled;
+					render.enabled = false;
+				}
+			}
+
 		if (potentialParallaxParent) 
 			potentialParallaxParent.SetActive(!doShow);
 	}
